c1df4bb9e963b01753828f42fa25080bb3566615
therishabhshrivastava <therishabhshrivastava@gmail.com>
2024-06-05 20:51:26 +0530

    Adding Project sections in Portfolio

--- script.js
+++ script.js
@@ -50,3 +50,142 @@ window.addEventListener("scroll", function(){
       }
     }
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    
+    function typeText(element, texts, speed) {
+        var index = 0;
+        var textIndex = 0;
+        var typingText = '';
+        var isDeleting = false;
+
+        
+        function type() {
+            var currentText = texts[textIndex];
+
+            if (isDeleting) {
+                typingText = currentText.substring(0, typingText.length - 1);
+            } else {
+                typingText = currentText.substring(0, typingText.length + 1);
+            }
+
+            element.textContent = typingText;
+
+            var typeSpeed = isDeleting ? speed / 2 : speed;
+
+            if (!isDeleting && typingText === currentText) {
+                typeSpeed = 1500;
+                isDeleting = true;
+            } else if (isDeleting && typingText === '') {
+                isDeleting = false;
+                textIndex = (textIndex + 1) % texts.length;
+            }
+
+            setTimeout(type, typeSpeed);
+        }
+
+        
+        type();
+    }
+
+  
+    var typingElement1 = document.querySelector(".typing");
+    typeText(typingElement1, ["Front-End Developer"], 100);
+
+    
+    var typingElement2 = document.querySelector(".typing-2");
+    typeText(typingElement2, ["Front-End Developer"], 100);
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var resumeBtn = document.getElementById("resumeBtn");
+
+    resumeBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var resumeUrl = "Resume_RishabhKumar.pdf";
+
+        var resumeWindow = window.open(resumeUrl, "_blank");
+
+        var downloadLink = document.createElement("a");
+        downloadLink.href = resumeUrl;
+        downloadLink.download = "Resume_RishabhKumar.pdf"; 
+        downloadLink.target = "_blank";
+        downloadLink.click();
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var resumeBtn = document.getElementById("about-resumeBtn");
+
+    resumeBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var resumeUrl = "Resume_RishabhKumar.pdf";
+
+        // var resumeWindow = window.open(resumeUrl, "_blank");
+
+        var downloadLink = document.createElement("a");
+        downloadLink.href = resumeUrl;
+        downloadLink.download = "Resume_RishabhKumar.pdf"; 
+        downloadLink.target = "_blank";
+        downloadLink.click();
+    });
+});
+
+
+document.addEventListener("visibilitychange", function() {
+    if (document.visibilityState === 'hidden') {
+      document.title = "Come Back to Portfolio!";
+    } else {
+      document.title = "Portfolio | Rishabh Kumar";
+    }
+});
+
+// $(document).ready(function(){
+//   $('.carousel').owlCarousel({
+//       margin: 20,
+//       loop: true,
+//       autoplay: true,
+//       autoplayTimeOut: 2000,
+//       autoplayHoverPause: true,
+//       responsive: {
+//           0:{
+//               items: 1,
+//               nav: false
+//           },
+//           600:{
+//               items: 2,
+//               nav: false
+//           },
+//           1000:{
+//               items: 3,
+//               nav: false
+//           }
+//       }
+//   });
+// });
+$(document).ready(function(){
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 1000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+  });
+});
